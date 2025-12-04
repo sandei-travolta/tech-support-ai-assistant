e055c3babdd5e60c477823c105645b307885749d
sandei-travolta <sandeitravolta@gmail.com>
2025-12-04 14:20:54 +0300

    spring boot initialization

---
+++ springboot-api/src/main/java/sandei/dev/ai_tech_assistant/controllers/AuthenticationContoller.java
@@ -0,0 +1,4 @@
+package sandei.dev.ai_tech_assistant.controllers;
+
+public class AuthenticationContoller {
+}
---
+++ springboot-api/src/main/java/sandei/dev/ai_tech_assistant/webhooks/TwiloWebHook.java
@@ -0,0 +1,4 @@
+package sandei.dev.ai_tech_assistant.webhooks;
+
+public class TwiloWebHook {
+}
