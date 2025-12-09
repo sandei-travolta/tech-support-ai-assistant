593067d4bb850b83c93996c218f9146a064f2bd2
sandei-travolta <sandeitravolta@gmail.com>
2025-12-09 18:22:33 +0300

    dashboard page

--- flutter-ui/lib/ui/dashboard/widgets/paginationCounter.dart
+++ flutter-ui/lib/ui/dashboard/widgets/paginationCounter.dart
@@ -6,7 +6,19 @@ class PaginationCounter extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      child: Text("1"),
+      child: Row(
+        spacing: 25.0,
+        mainAxisAlignment: .center,
+        children: [
+          Icon(
+              size: 15.0,
+              Icons.arrow_back),
+          Text("1"),
+          Icon(
+              size: 15.0,
+              Icons.arrow_forward)
+        ],
+      ),
     );
   }
 }
