b81c349f974a4e4a82c60f0b85a492c42340205b
sandei-travolta <sandeitravolta@gmail.com>
2026-02-04 22:47:07 +0300

    jwt implementation

--- springboot-api/src/main/java/sandei/dev/ai_tech_assistant/controllers/AuthenticationContoller.java
+++ springboot-api/src/main/java/sandei/dev/ai_tech_assistant/controllers/AuthenticationContoller.java
@@ -1,9 +1,16 @@
 package sandei.dev.ai_tech_assistant.controllers;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import sandei.dev.ai_tech_assistant.dTOs.authentication.LoginRequestDto;
 
 @RestController
 @RequestMapping("/auth")
 public class AuthenticationContoller {
+    @PostMapping("/login")
+    public ResponseEntity<Integer> login(LoginRequestDto requestDto){
+        return ResponseEntity.ok(1);
+    }
 }
